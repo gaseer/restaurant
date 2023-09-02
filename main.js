e710c306d054780578731174a6fef8f87a84dca8
Shiyas Cholamukhath <cmshiyas007@gmail.com>
2023-09-02 11:02:00 +1000

    Revert "comment out code on target class"

--- main.js
+++ main.js
@@ -115,57 +115,56 @@ $(document).ready(function ($) {
     var parallaxInstance = new Parallax(scene);
 
 
+});
+
+jQuery(window).on('load', function () {
+    $('body').removeClass('body-fixed');
+
+    //activating tab of filter
+    let targets = document.querySelectorAll(".filter");
+    let activeTab = 0;
+    let old = 0;
+    let dur = 0.4;
+    let animation;
+
+    for (let i = 0; i < targets.length; i++) {
+        targets[i].index = i;
+        targets[i].addEventListener("click", moveBar);
+    }
+
+    // initial position on first === All 
+    gsap.set(".filter-active", {
+        x: targets[0].offsetLeft,
+        width: targets[0].offsetWidth
+    });
+
+    function moveBar() {
+        if (this.index != activeTab) {
+            if (animation && animation.isActive()) {
+                animation.progress(1);
+            }
+            animation = gsap.timeline({
+                defaults: {
+                    duration: 0.4
+                }
+            });
+            old = activeTab;
+            activeTab = this.index;
+            animation.to(".filter-active", {
+                x: targets[activeTab].offsetLeft,
+                width: targets[activeTab].offsetWidth
+            });
+
+            animation.to(targets[old], {
+                color: "#0d0d25",
+                ease: "none"
+            }, 0);
+            animation.to(targets[activeTab], {
+                color: "#fff",
+                ease: "none"
+            }, 0);
+
+        }
+
+    }
 });
-
-
-// jQuery(window).on('load', function () {
-//     $('body').removeClass('body-fixed');
-
-//     //activating tab of filter
-//     let targets = document.querySelectorAll(".filter");
-//     let activeTab = 0;
-//     let old = 0;
-//     let dur = 0.4;
-//     let animation;
-
-//     for (let i = 0; i < targets.length; i++) {
-//         targets[i].index = i;
-//         targets[i].addEventListener("click", moveBar);
-//     }
-
-//     // initial position on first === All 
-//     gsap.set(".filter-active", {
-//         x: targets[0].offsetLeft,
-//         width: targets[0].offsetWidth
-//     });
-
-//     function moveBar() {
-//         if (this.index != activeTab) {
-//             if (animation && animation.isActive()) {
-//                 animation.progress(1);
-//             }
-//             animation = gsap.timeline({
-//                 defaults: {
-//                     duration: 0.4
-//                 }
-//             });
-//             old = activeTab;
-//             activeTab = this.index;
-//             animation.to(".filter-active", {
-//                 x: targets[activeTab].offsetLeft,
-//                 width: targets[activeTab].offsetWidth
-//             });
-
-//             animation.to(targets[old], {
-//                 color: "#0d0d25",
-//                 ease: "none"
-//             }, 0);
-//             animation.to(targets[activeTab], {
-//                 color: "#fff",
-//                 ease: "none"
-//             }, 0);
-
-//         }
-
-//     }
-// });
